ba956fa54d062a8280b81fc14e22a8045b251899
paput11@gmail.com <paput11@gmail.com>
2023-01-25 19:51:41 -0300

    estapas resueltas

--- 12_CLASE/app.js
+++ 12_CLASE/app.js
@@ -1,11 +1,10 @@
-/* requerir módulo autos */
 let autosImportados = require("./autos");
 
 let concesionaria = {
    autos: autosImportados,
  
-    buscarAuto: function autobuscado(patente) {
-        return this.autos.find(auto => auto.patente === patente) ?? null;
+   buscarAuto: function autobuscado(patente) {
+        return this.autos.find(auto => auto.patente === patente)?? null;
     },
 
    venderAuto: function autobuscado(patente){
@@ -15,11 +14,37 @@ let concesionaria = {
       }
    },
 
-   autosParaLaVenta: function disponible(){
-    let disponibles = this.autos.filter( this.autos.vendido == false);
-    console.log(disponibles);
-   }
+    autosParaLaVenta: function (){
+     let autosDisponibles = this.autos.filter(auto => auto.vendido == false);
+     return autosDisponibles
+   },
 
-};
+   autosNuevos: function (){
+     return this.autosParaLaVenta().filter(auto => auto.km < 100 );
+   },
 
-console.log(concesionaria.autosParaLaVenta());
+   listaDeVentas: function(){
+     let autosVendidos = this.autos.filter (auto => auto.vendido === true);
+     let listaPrecio = autosVendidos.map (auto => auto.precio)
+     return listaPrecio;
+   },
+
+   totalDeVentas: function(){
+      let totalSuma = this.listaDeVentas().reduce((acumulador, suma) => acumulador + suma, 0);
+      return totalSuma;
+   },
+   
+  puedeComprar : function(auto,persona){
+   if (auto.precio <= persona.capacidadDePagoTotal && auto.precio/auto.cuotas <=persona.capacidadDePagoEnCuotas){
+      return true;
+   }else {return false}
+  },
+  
+   autosQuePuedeComprar : function (persona){
+   let autosParaLaVenta = this.puedeComprar;
+   let losQuePuedenComprar = this.autosParaLaVenta().filter(auto =>{
+      return autosParaLaVenta (auto,persona)
+  })
+  return losQuePuedenComprar}
+   
+}
